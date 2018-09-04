c7fcb19ab259330cbdf829a2e31e2646736a52e8
Blake Jordan <blake.jordan@mediware.com>
2018-09-04 12:14:51 -0500

    unneccessary code cleanup

--- src/app/board/board/board.component.ts
+++ src/app/board/board/board.component.ts
@@ -7,36 +7,6 @@ import { Router } from '@angular/router';
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.css']
 })
-export class BoardComponent implements OnInit {
+export class BoardComponent {
 
-  boardFormGroup: FormGroup;
-
-  constructor(private formBuilder: FormBuilder, public router: Router) {
-    this.boardFormGroup = this.formBuilder.group({});
-  }
-
-  ngOnInit(): void {
-    this.createForm();
-  }
-
-  createForm(): void {
-    this.boardFormGroup = this.formBuilder.group({
-      boardWidth: ['', Validators.compose([Validators.min(2), Validators.required])],
-      boardHeight: ['', Validators.compose([Validators.min(2), Validators.required])],
-      totalMines: ['', Validators.compose([Validators.min(1), Validators.required, Validators.pattern('^\\d+$')])],
-    });
-  }
-
-  validateForm(): void {
-    if (!this.boardFormGroup.valid) {
-      alert('Board size must be at least 2x2 with at least 1 mine');
-    } else {
-      // alert ('succeeded');
-      console.log('works');
-      const routeSuccess = this.router.navigate(['board']);
-      if (routeSuccess) {
-        console.log('success');
-      }
-    }
-  }
 }
