c1fa59785055f3c14b57d1b5306c799108182937
Blake Jordan <blake.jordan@mediware.com>
2018-09-06 13:56:20 -0500

    shared board data with board component

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,14 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, SimpleChange } from '@angular/core';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.css']
 })
-export class BoardComponent implements OnInit {
+export class BoardComponent implements OnChanges {
+  @Input() boardWidth: number;
+  @Input() boardHeight: number;
+  @Input() mines: number;
+  changeLog: string[] = [];
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges(changes: {[propKey: string]: SimpleChange}) {
+    let log: string[] = [];
+    for (let propName in changes) {
+      let changedProp = changes[propName];
+      let to = JSON.stringify(changedProp.currentValue);
+      if (changedProp.isFirstChange()) {
+        log.push(`Initial value of ${propName} set to ${to}`);
+      } else {
+        let from = JSON.stringify(changedProp.previousValue);
+        log.push(`${propName} changed from ${from} to ${to}`);
+      }
+    }
+    this.changeLog.push(log.join(', '));
   }
 }
