afb0d9b45956d3886792e3e2be6a055fb6f53c81
McKenna Groves <mckenna.groves97@gmail.com>
2018-09-06 17:47:16 -0500

    Added basic code outline to createBoard method

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -22,6 +22,30 @@ export class BoardComponent implements OnChanges {
 
   createBoard(boardWidth, boardHeight, mines)
   {
-     //Create board from user inputs here
+    /*
+    var board = new Array(boardWidth);
+    for (var i = 0; i < boardWidth; i++)
+    {
+      board[i] = new Array(boardHeight);//create 2D array
+    }
+     for (var i = 0; i < boardWidth; i++)
+     {
+       for (var j = 0; j < boardHeight; j++)
+       {
+         //create tile object
+       }
+     }
+     var count = 0;
+     while(count < mines)
+     {
+       var mine_row = Math.floor(Math.random() * Math.floor(boardWidth));//random number generator
+       var mine_col = Math.floor(Math.random() * Math.floor(boardHeight));
+      if (tile @ (mine_row,mine_col) !is_mine)
+      {
+        is_mine = true;//if there isn't already a mine at that spot, place mine
+      }
+      count++;
+     }
+     */
   }
 }
