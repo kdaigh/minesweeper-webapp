e66593a7b884a2ddc7d4d98f0adc391a532d6752
Blake Jordan <blake.jordan@mediware.com>
2018-09-08 14:18:36 -0500

    Added documentation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { UserInputComponent } from './user-input/user-input.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material';
 import { BoardComponent } from './board/board.component';
+import { DocumentationComponent } from './documentation/documentation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserInputComponent,
-    BoardComponent
+    BoardComponent,
+    DocumentationComponent
   ],
   imports: [
     BrowserModule,
