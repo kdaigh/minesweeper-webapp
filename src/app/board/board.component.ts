82c3ac4d94577e8c86d436507465bec3325e67e6
Austin Wildman <austinwildman7@gmail.com>
2018-09-10 12:23:56 -0600

    Not much change just updates

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnChanges, Input } from '@angular/core';
-import { SimpleTimer } from 'ng2-simple-timer';
+import { Component, OnChanges, Input, SimpleChange } from '@angular/core';
 import { minefield } from '../models/minefield';
 
 @Component({
@@ -13,45 +12,12 @@ export class BoardComponent implements OnChanges {
   @Input() mines: number;
   flagCount;
   timerCount;
-  timerID: string;
+  changeLog: string[] = [];
 
-  constructor(private st: SimpleTimer) {
-   }
+  constructor() { }
 
   ngOnChanges() {
-    this.generate_table();
     this.createBoard();
-  }
-
-  createBoard()
-  {
-    console.log("createBoard() called");
-    this.timerCount = 0; //Reset timer count
-    if (this.mines != 0) //Not page startup
-    {
-      const mineField = new minefield(this.boardHeight, this.boardWidth);
-      this.placeAllMines(minefield);
-      this.placeAllNumbers(minefield);
-      this.flagCount = this.mines;
-      if (this.timerID == undefined) //If timer has not been subscribed
-      {
-        this.st.newTimer('Timer', 1);
-        this.subscribeTimer();
-      }
-    }
-  }
-
-  subscribeTimer()
-  {
-    if (this.mines == 0) //Page startup
-    {
-      this.st.unsubscribe(this.timerID);
-      this.timerID = undefined;
-    }
-    else
-    {
-      this.timerID = this.st.subscribe('Timer', () => this.updateTimer());
-    }
   }
 
   updateTimer()
@@ -59,32 +25,52 @@ export class BoardComponent implements OnChanges {
     this.timerCount++;
   }
 
-  //returns the current tile you are on.
-  getTile()
+  
+  createBoard()
   {
-    return;
+    const mineField = new minefield(this.boardHeight, this.boardWidth);
+    if (this.mines == 0) //Page startup
+    {
+      this.flagCount = "";
+    }
+    else //"New Game" click
+    {
+      this.flagCount = this.mines;
+      //Start timer
+    }
+    this.placeAllMines(minefield);
+    this.placeAllNumbers(minefield);
+  }
+
+  //returns the current tile you are on.
+  getTile(mineField)
+  {
+    //return minefield.rows
   }
 
   //Places a random mine on the board
-  placeMine(ninefield)
+  placeMine(mineField)
   {
-
+    /*var row = Math.round(Math.random() * 8);
+    var col = Math.round(Math.random() * 8);
+    var position = getTile(minefield);
+    position.content = "bomb";*/
   }
 
   //Loop calls place mine to populate the board with random mines
-  placeAllMines(minefield)
+  placeAllMines(mineField)
   {
 
   }
 
   //Calculate what number to put in the tile.
-  placeNumber(minefield, row, col)
+  placeNumber(mineField, row, col)
   {
 
   }
 
   //Loop calls placeNumber to fill in all required tiles with numbers.
-  placeAllNumbers(minefield)
+  placeAllNumbers(mineField)
   {
 
   }
@@ -99,41 +85,6 @@ export class BoardComponent implements OnChanges {
   boardComtroller($scope)
   {
 
-  }
-
-  generate_table() {
-    // get the reference for the body
-    var body = document.getElementsByName("minefield")[0];
-   
-    // creates a <table> element and a <tbody> element
-    var tbl = document.createElement("table");
-    var tblBody = document.createElement("tbody");
-   
-    // creating all cells
-    for (var i = 0; i < this.boardHeight; i++) {
-      // creates a table row
-      var row = document.createElement("tr");
-   
-      for (var j = 0; j < this.boardWidth; j++) {
-        // Create a <td> element and a text node, make the text
-        // node the contents of the <td>, and put the <td> at
-        // the end of the table row
-        var cell = document.createElement("td");
-        var cellText = document.createTextNode("cell in row "+i+", column "+j);
-        cell.appendChild(cellText);
-        row.appendChild(cell);
-      }
-   
-      // add the row to the end of the table body
-      tblBody.appendChild(row);
-    }
-   
-    // put the <tbody> in the <table>
-    tbl.appendChild(tblBody);
-    // appends <table> into <body>
-    body.appendChild(tbl);
-    // sets the border attribute of tbl to 2;
-    tbl.setAttribute("border", "2");
   }
 
 }
