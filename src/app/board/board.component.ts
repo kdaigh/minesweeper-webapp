331f31a7b8d43d530f6cee510731d0e321a1d295
Austin Wildman <austinwildman7@gmail.com>
2018-09-13 17:07:55 -0600

    Update to board

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnChanges, Input } from '@angular/core';
 import { SimpleTimer } from 'ng2-simple-timer';
+import { board } from '../models/board';
 import { tile } from '../models/tile';
-import { board } from '../models/board'
 
 @Component({
   selector: 'app-board',
@@ -114,4 +114,39 @@ export class BoardComponent implements OnChanges {
   {
 
   }
+  generate_table() {
+    // get the reference for the body
+    var body = document.getElementsByName("minefield")[0];
+   
+    // creates a <table> element and a <tbody> element
+    var tbl = document.createElement("table");
+    var tblBody = document.createElement("tbody");
+   
+    // creating all cells
+    for (var i = 0; i < this.rowCount; i++) {
+      // creates a table row
+      var row = document.createElement("tr");
+   
+      for (var j = 0; j < this.columnCount; j++) {
+        // Create a <td> element and a text node, make the text
+        // node the contents of the <td>, and put the <td> at
+        // the end of the table row
+        var cell = document.createElement("td");
+        var obj = document.createElement("tileObj");
+        cell.appendChild(obj);
+        row.appendChild(cell);
+      }
+   
+      // add the row to the end of the table body
+      tblBody.appendChild(row);
+    }
+   
+    // put the <tbody> in the <table>
+    tbl.appendChild(tblBody);
+    // appends <table> into <body>
+    body.appendChild(tbl);
+    // sets the border attribute of tbl to 2;
+    tbl.setAttribute("border", "2");
+  }
+
 }
